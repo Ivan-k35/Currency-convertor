e4ee6fe474f300cec9345d33d071b79bdc41137f
Ivan-k35 <ivan.koshkin35@gmail.com>
2022-08-18 12:33:39 +0300

    Добавил API, обновил views.py

--- exchange_app/views.py
+++ exchange_app/views.py
@@ -1,13 +1,41 @@
 from django.shortcuts import render
-
-# Create your views here.
+import requests
 
 
 def exchange(request):
-    name = 'Ivan'
-
-    context = {
-        'name': name
+    url = 'https://api.exchangerate.host/latest'
+    params = {
+        'base': 'USD',
+        'symbols': ['USD,EUR,RUB']
     }
 
-    return render(request, 'exchange_app/index.html', context)
+    response = requests.get(url, params=params).json()
+    currencies = response.get('rates')
+
+    if request.method == 'GET':
+
+        context = {
+            'currencies': currencies
+        }
+
+        return render(request, 'exchange_app/index.html', context)
+
+    if request.method == 'POST':
+        if request.POST.get('from-amount'):
+            from_amount = float(request.POST.get('from-amount'))
+        else:
+            from_amount = 0
+        from_curr = request.POST.get('from-curr')
+        to_curr = request.POST.get('to-curr')
+
+        converted_amount = round((currencies[to_curr] / currencies[from_curr]) * float(from_amount), 2)
+
+        context = {
+            'from_curr': from_curr,
+            'to_curr': to_curr,
+            'from_amount': from_amount,
+            'currencies': currencies,
+            'converted_amount': converted_amount
+        }
+
+        return render(request, 'exchange_app/index.html', context)
